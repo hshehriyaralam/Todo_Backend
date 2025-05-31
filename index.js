765d27f334abffe48ad8de953733c152196de852
hshehriyaralam <hshehriyaralam@gmail.com>
2025-05-31 14:16:15 +0500

    change CORS URL

--- index.js
+++ index.js
@@ -10,14 +10,17 @@ dotenv.config({
 const PORT = process.env.PORT || 3000
 
 
+
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
+
 app.use(cors({
-    origin : process.env.CORS_ORIGIN,
-    credentials : true,
-}))
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
+
 
 app.use('/api/todos', todoRouter)
 
